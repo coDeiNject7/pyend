06f9aec1a7c7d4335f4eae139816da5d9dc206ef
pavan <66791294+coDeiNject7@users.noreply.github.com>
2026-05-29 12:03:13 +0530

    Refactor user routes for better organization and new features

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from app.schemas.user import UserCreate, UserResponse, UserDelete, UserUpdate
-from app.services.user_service import create_user, get_users, delete_user, update_user
+from app.schemas.user import (
+    UserCreate, 
+    UserResponse, 
+    UserDelete, 
+    UserUpdate, 
+    UserUpdatePartially)
+from app.services.user_service import (
+    create_user, 
+    get_users, 
+    delete_user, 
+    update_user, 
+    get_a_user, 
+    update_user_partially)
 from app.db.database import SessionLocal
 
 router = APIRouter()
@@ -16,30 +27,45 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/users", response_model = UserResponse)
+@router.get("/get-a-user", response_model=UserResponse)
+def get_single_user(
+    user_id:int,
+    db : Session = Depends(get_db)
+):
+    return get_a_user(db, user_id)
+
+@router.get("/get-all-users", response_model = list[UserResponse])
+def get_all_users(
+    db : Session = Depends(get_db)    
+):
+    return get_users(db)
+
+@router.post("/create-user", response_model = UserResponse)
 def create_new_users(
     user : UserCreate,
     db : Session = Depends(get_db)
 ):
     return create_user(db, user.name, user.email)
 
-@router.get("/users", response_model = list[UserResponse])
-def get_all_users(
-    db : Session = Depends(get_db)    
-):
-    return get_users(db)
-
-@router.delete("/users/{user_id}", response_model = UserDelete)
+@router.delete("/delete-a-user/{user_id}", response_model = UserDelete)
 def delete_a_user(
     user_id:int,
     db:Session = Depends(get_db)
 ):
     return  delete_user(db, user_id)
 
-@router.put("/users/{user_id}", response_model = UserResponse)
+@router.put("/update-a-user/{user_id}", response_model = UserResponse)
 def update_a_user(
     user_id:int,
     user:UserUpdate,
     db:Session = Depends(get_db)
 ):
     return update_user(db, user.name, user.email, user_id)
+
+@router.patch("/update-partially/{user_id}", response_model=UserResponse)
+def update_partially(
+    user_id:int,
+    request:UserUpdatePartially,
+    db:Session=Depends(get_db)
+):
+    return update_user_partially(db, user_id, request)
