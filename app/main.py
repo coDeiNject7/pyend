1fe3986408acc8eb90dd6e91f27f5b6467ef415f
pavan <66791294+coDeiNject7@users.noreply.github.com>
2026-05-29 12:01:50 +0530

    Add title to FastAPI app and update users router

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from app.db.database import engine, Base
 from app.api.routes.auth import router as auth_router
 from app.api.routes import users_router
 
-app = FastAPI()
+app = FastAPI(title="pavs'end")
 
 #Create Tables
 #Base.metadata.create_all(bind=engine)
@@ -16,5 +16,5 @@ def root():
 
 app.include_router(auth_router)
 
-#GET, POST, UPDAT AND DELETE API for Users
-app.include_router(users_router)
+#GET, POST, UPDATE AND DELETE API for Users
+app.include_router(users_router, prefix="/api/users", tags=["User"])
